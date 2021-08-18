eb045e8dbc324f194c8ff00d4eefd5f94564fb3e
yasayup <jasayup342@gmail.com>
2021-08-18 20:11:19 -0300

    listo para subir a heroku

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "/public")));
 
-app.listen(3000, () => { });
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Se prendió!");
+});
+
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "views/home.html"));
