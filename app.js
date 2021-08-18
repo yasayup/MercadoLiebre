3bc17d34e36ac86c4a8e5884b7cd4a0d16b667b2
yasayup <jasayup342@gmail.com>
2021-08-18 19:31:26 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.listen(3000, () => { });
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/home.html"));
+});
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/register.html"));
+});
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "views/login.html"));
+});
